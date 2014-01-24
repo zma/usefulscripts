6120e179c1ea655ac8d24908f44d35493ad60907
Eric Zhiqiang Ma <eric@zhiqiangma.com>
2014-01-24 15:46:38 +0800

    add install-scala.sh

--- script/install-scala-2.9.2.sh
+++ script/install-scala-2.9.2.sh
@@ -1,18 +1,6 @@
-#!/bin/bash
 
 # Install scala 2.9.2
 # Author: Zhiqiang Ma http://www.zhiqiangma.com
 
-echo "You need to run this as root or by sudo. 
-Enter to continue. Ctrl-C to abort."
-
-read input
-
-wget http://www.scala-lang.org/downloads/distrib/files/scala-2.9.2.tgz
-tar xf scala-2.9.2.tgz
-
-mv scala-2.9.2 /opt/
-echo "export PATH=/opt/scala-2.9.2/bin/:$PATH" > /etc/profile.d/scala-2.9.2.sh
-
-rm -f scala-2.9.2.tgz
+# Please check install-scala.sh
 
---
+++ script/install-scala.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install scala of version $1
+# Author: Zhiqiang Ma (http://www.zhiqiangma.com)
+
+if [ $# < 2 ]
+then
+    echo "Usage: $0 version"
+    exit 1
+fi
+
+ver=$1
+
+echo "You are to install scala $ver to /opt/"
+echo ""
+echo "You need to run this as root or by sudo. 
+Enter to continue. Ctrl-C to abort."
+
+read input
+
+wget http://www.scala-lang.org/files/archive/scala-$ver.tgz
+tar xf scala-$ver.tgz -C /opt/
+
+echo "export PATH=/opt/scala-$ver/bin/:$PATH" > /etc/profile.d/scala.sh
+
+rm -f scala-$ver.tgz
+
+echo "Scala $ver is installed."
+echo "Please login again and try to run `scala` and test it"
+echo "Enjoy!"
+
