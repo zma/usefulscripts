a8caf17788029b151c96cc623e3b5c4a2cd2e908
Eric Zhiqiang Ma <zma@ericzma.com>
2014-06-20 12:29:28 +0800

    add download-build-install-emacs.sh

---
+++ script/download-build-install-emacs.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+# Install emacs of version $1
+# Author: Eric Zhiqiang Ma (http://www.ericzma.com)
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 version"
+    exit 1
+fi
+
+ver=$1
+
+wget http://ftp.gnu.org/gnu/emacs/emacs-$ver.tar.gz && \
+tar xf emacs-$ver.tar.gz && \
+cd emacs-$ver && \
+./configure  --with-xpm=no --with-jpeg=no --with-gif=no --with-tiff=no && \
+make && \
+sudo make install
