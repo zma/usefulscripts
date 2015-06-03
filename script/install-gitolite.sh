9b0d0cd40a3dc9af390ee5e04b993edbff498530
Eric Zhiqiang Ma <zma@ericzma.com>
2015-06-03 14:19:39 +0800

    install gitolite

---
+++ script/install-gitolite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install gitolite in one command
+# For how to use it, please check
+# http://www.systutorials.com/5323/how-to-set-up-gitolite-git-server-a-ten-minute-tutorial/
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 admin_pub_key"
+    exit 1
+fi
+
+ADMIN_KEY=$1
+
+cd ~
+mkdir bin
+echo "export PATH=$HOME/bin/:$PATH" >> .bashrc
+
+git clone https://github.com/sitaramc/gitolite
+gitolite/install -to $(realpath $HOME/bin/)
+bin/gitolite setup -pk $ADMIN_KEY
