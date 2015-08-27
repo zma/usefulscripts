14758e4a79384a563e9513f5fb9cf5843ac46fd1
Eric Zhiqiang Ma <zma@ericzma.com>
2015-08-27 12:33:48 +0800

    add check-cmd-chage and doafterchagne

--- script/check-cmd-change.bash
+++ script/check-cmd-change.bash
@@ -1,34 +1,3 @@
 #!/bin/bash
 
-# Eric Zhiqiang Ma (http://www.ericzma.com)
-# Example
-# ./check-cmd-change.bash "node-status" "(for i in 10.0.3.{2..50}; do echo $i; ssh $i 'dmesg | grep error'; done)" 'mailx -S smtp="smtp://smtp.ust.hk" -s "dmesg error monitor @wiles" -r zma@connect.ust.hk zma@connect.ust.hk'
-
-if [[ $# < 2 ]]; then
-    echo "Usage: $0 id 'watched cmd' 'trigger cmd'"
-    exit 1;
-fi
-
-id=$1
-cmd=$2
-trigger=$3
-
-oldfile=/tmp/check-cmd-change-$id-old-$USER
-newfile=/tmp/check-cmd-change-$id-new-$USER
-difffile=/tmp/check-cmd-change-$id-diff-$USER
-
-for f in $oldfile $newfile; do
-    if [ ! -f $f ]; then
-        touch $f
-    fi
-done
-
-mv $newfile $oldfile
-
-eval $cmd >$newfile
-
-diff -u ${oldfile} ${newfile} >$difffile
-if [[ $? == 1 ]]; then
-    echo "===============" >>$difffile
-    cat $difffile $newfile | eval $trigger
-fi
+echo "Please use check-cmd-change.sh instead."
