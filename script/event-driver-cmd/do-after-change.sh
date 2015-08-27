8ebfc08c6da4ba9644f3572d7cbefc2e6e07fc7f
Eric Zhiqiang Ma <zma@ericzma.com>
2015-08-27 12:38:36 +0800

    add more commands

---
+++ script/event-driver-cmd/do-after-change.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Eric Zhiqiang Ma (http://www.ericzma.com)
+
+# Example
+#   do-after-change.sh make *.c *.h Makefile
+
+cmdfile=/tmp/doafterchanges
+touch $cmdfile
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 cmd watched_files"
+    exit 1
+fi
+
+which inotifywait 2>&1 >/dev/null
+
+if [ "$?" == "1" ]; then
+    echo "You need inotifywait installed"
+    exit 1
+fi
+
+cmd=$1
+shift
+files=$*
+
+function exec_cmd () {
+    echo "[`date`] >>>> Run: $cmd"
+    eval $cmd
+    echo "[`date`] >>>> Exit code: $?"
+}
+
+# do it first
+exec_cmd
+
+while true; do
+    # inotifywait -t 60 -q -r $files
+    inotifywait -q -r $files $cmdfile
+    exec_cmd
+done
