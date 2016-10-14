b3cc6d8edd32a0582775bae9fb28ca8bff7bcbf1
Eric Zhiqiang Ma <zma@ericzma.com>
2016-10-14 14:10:01 +0800

    add install-wine-i686-centos7

---
+++ script/install-wine-i686-centos7.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Author: Eric Zhiqiang Ma (zma@ericzma.com)
+
+# Install
+
+ver=1.8.5
+
+echo "Uninstall old wine64 if you have installed it..."
+
+yum erase wine wine-*
+
+echo "Install wine building tools..."
+
+yum groupinstall 'Development Tools'
+
+yum install libjpeg-turbo-devel libtiff-devel
+
+echo "Download and unpack the wine source package..."
+
+cd /usr/src
+wget http://dl.winehq.org/wine/source/1.8/wine-${ver}.tar.bz2 -O wine-${ver}.tar.bz2
+tar xjf wine-${ver}.tar.bz2
+
+echo "Build wine..."
+cd wine-${ver}/
+mkdir wine32 wine64
+
+echo "   build wine64..."
+cd wine64
+../configure --enable-win64
+make -j 4
+
+echo "   build wine32..."
+cd ../wine32
+../configure --with-wine64=../wine64
+make -j 4
+
+echo "Install wine..."
+echo "   install wine32..."
+make install
+
+echo "   install wine64..."
+cd ../wine64
+make install
+
+echo "Congratulation! All are done. Enjoy!"
+
+
+# # Uninstall
+# cd /usr/src/wine-${ver}/wine32
+# make uninstall
+# cd /usr/src/wine-${ver}/wine64
+# make uninstall
+
