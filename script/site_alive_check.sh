25269397bf9dc17e300c1cbc95ff74fbfa3b21be
Eric Ma <zma@ericzma.com>
2020-03-28 13:26:33 +0800

    site_alive_check: updated logic

--- script/site_alive_check.sh
+++ script/site_alive_check.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-localdir=/tmp/fclose.com
-testurl=http://fclose.com/
-testfile=index.html
+# You may use cron to trigger this regularly
+# and add actions to be triggered for notifications.
 
-testfileaddr=${localdir}/${testfile}
-mkdir -p $localdir
-rm -f ${localdir}/${testfile}
+localdir=/tmp/site-alive-check
+testfileaddr=${localdir}/out
+testurl=https://www.systutorials.com
 
-wget --tries=3 --timeout=10 ${testurl} -O ${testfileaddr}
-
-if [[ -f ${testfileaddr}  ]]
-then
-    date;
-    echo "ok";
-else
-    date | mailx -S smtp="smtp://smtp.ust.hk" -s "fclose.com test failed." -r "zma@fclose.com" zma@fclose.com
+if [[ "$1" != "" ]]; then
+  testurl="$1"
 fi
 
+mkdir -p $localdir
+rm -f ${testfileaddr}
+
+wget --tries=3 --timeout=10 "${testurl}" -O ${testfileaddr}
+
+# you may send email too using `mailx`
+# e.g. https://www.systutorials.com/sending-email-from-mailx-command-in-linux-using-gmails-smtp/
+#      https://www.systutorials.com/sending-email-using-mailx-in-linux-through-internal-smtp/
+
+if [[ -f ${testfileaddr} ]]; then
+  size=$(wc -c ${testfileaddr} | cut -d' ' -f1)
+  if [[ "$size" != "0" ]]; then
+    echo "[`date`] ${testurl} => ok"
+    exit 0
+  fi
+fi
+
+echo "[`date`] ${testurl} => failed"
+exit 1
