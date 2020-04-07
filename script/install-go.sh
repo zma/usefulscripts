ede1eefa07a37b7eed82e649b00d7b46ac23997a
Eric Ma <zma@ericzma.com>
2020-04-08 00:10:55 +0800

    install-go avoid name collision

--- script/install-go.sh
+++ script/install-go.sh
@@ -20,7 +20,6 @@ fi
 
 majorver=$(echo ${version} | cut -d'.' -f1,2)
 pkgfile=go${version}.linux-amd64.tar.gz
-wget https://dl.google.com/go/${pkgfile} -O ${pkgfile}
 
 if [[ -e /usr/local/go-${majorver} ]]; then
   echo "ERROR: Folder/file /usr/local/go-${majorver} exists."
@@ -30,8 +29,12 @@ fi
 tmpdir=tmp-go-${version}-$(date +%s)
 mkdir -p ${tmpdir}
 cd ${tmpdir}
+
+wget https://dl.google.com/go/${pkgfile} -O ${pkgfile}
+
 tar xf ${pkgfile}
 sudo mv go /usr/local/go-${majorver}
+
 cd ..
 rmdir ${tmpdir}
 
