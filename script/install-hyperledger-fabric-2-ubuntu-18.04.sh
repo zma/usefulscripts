6942e5d09a1bb891562af4f993d06602c805ed99
Eric Ma <zma@ericzma.com>
2020-04-07 20:54:29 +0800

    fix bug in install fabric

--- script/install-hyperledger-fabric-2-ubuntu-18.04.sh
+++ script/install-hyperledger-fabric-2-ubuntu-18.04.sh
@@ -13,10 +13,11 @@
 
 set -o errexit
 mydir=$(dirname $0)
+set -x
 
-echo "Set up Go ..."
-if [[ -e /usr/local/go-${majorver} ]]; then
-  echo "Warning: Folder/file /usr/local/go-${majorver} exists. It seems you have go 1.13 installed. Skip installation."
+echo "Set up Go 1.13.9 ..."
+if [[ -e "/usr/local/go-1.13" ]]; then
+  echo "Warning: Folder/file /usr/local/go-1.13 exists. It seems you have go 1.13 installed. Skip installation."
 else
   $mydir/install-go.sh 1.13.9
 fi
@@ -26,6 +27,7 @@ grep 'export GOPATH=$HOME/go' ~/.bashrc && bashrcupdated=0 || bashrcupdated=1
 
 if [[ "$bashrcupdated" == "1" ]]; then
   echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+  echo 'export PATH=$GOPATH/bin:$PATH' >> ~/.bashrc
 fi
 
 source ~/.bashrc
@@ -35,13 +37,19 @@ sudo apt install git curl wget docker docker-compose nodejs npm
 sudo npm install npm@5.6.0 -g
 sudo usermod -aG docker $USER
 
-newgrp docker
+echo "Versions of software:"
+docker --version
+docker-compose --version
+npm --version
+go version
 
+cd $GOPATH
 echo "Install Hyperledger Fabric 2.0.1 binaries and coker images ..."
-curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.0.1 1.4.6 0.4.18
+sg docker -c "curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.0.1 1.4.6 0.4.18 -s"
 
 echo "Now start test-network ..."
-go get -u github.com/hyperledger/fabric-samples
-cd $GOPATH/src/github.com/hyperledger/fabric-samples/test-network/
-
+go get -u github.com/hyperledger/fabric-samples || rt=1
+cd $GOPATH/src/github.com/hyperledger/fabric-samples
+git checkout a026a4ffbfcf69f33a2a25cd71c5a776ca2fdda5
+sg docker -c "cd $GOPATH/src/github.com/hyperledger/fabric-samples/test-network && ./network.sh up"
 
