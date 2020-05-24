59948c73ab2d1d686e35f3e25ba92dbc651c8261
Eric Ma <zma@ericzma.com>
2020-05-24 17:59:22 +0800

    specify different result saving

--- script/save-chrome-bookmarks.sh
+++ script/save-chrome-bookmarks.sh
@@ -11,6 +11,7 @@
 
 profileemail=$1
 resultpath=$2
+
 if [[ "$profileemail" == "" || "$resultpath" == "" ]]; then
   echo "Usage: $0 <email> <resultpath>"
   exit 1
@@ -26,12 +27,17 @@ if [[ ! -d "$profiledir" ]]; then
   exit 3
 fi
 
-prefpath=""
+if [[ "$resultpath" == "-" ]]; then
+  savecmd="cat"
+else
+  savecmd="cat > $resultpath"
+fi
+
 for d in "${profiledir}"/*; do
   if [[ -f "$d/Preferences" ]]; then
     email=$(jq '.account_info[0].email' "$d/Preferences" | tr -d '"')
     if [[ "$email" == "$profileemail" ]]; then
-      cat "$d/Bookmarks" > "$resultpath"
+      cat "$d/Bookmarks" | "$savecmd"
       exit 0
     fi
   else
